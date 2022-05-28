f9fcb62201ec7d1117704e795810002caf397fd7
Ben Platten <platten.ben@gmail.com>
2022-05-28 15:25:55 +0200

    testing on harder schedules

--- dev/main.py
+++ dev/main.py
@@ -6,17 +6,26 @@ import torch.optim as optim
 from datetime import datetime
 from util import plot_scores, plot_learning_curve
 
-now = datetime.now().strftime("%Y-%m-%d-%H:%M")
+start=datetime.now()
+now = datetime.now().strftime("%Y%m%d%H:%M")
+p = "pool_0001"
+s = "schedule_0003"
 
-pool, schedule = pd.read_csv('dev/schedules/simple_pool.csv',dtype={'employee_id':'str'}), \
-                 pd.read_csv('dev/schedules/simple_schedule.csv',dtype={'shift_id':'str'})
+pool, schedule = pd.read_csv(f'dev/pools/{p}.csv',dtype={'employee_id':'str'}), \
+                 pd.read_csv(f'dev/schedules/{s}.csv',dtype={'shift_id':'str'})
 
 env = SchedulingEnv(pool, schedule)
 
+#print(env.shift_features)
+print(env.count_workers)
+print(env.count_shifts)
+#print(env.state) 
+#print(env.action_space.sample())
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-encoder = Encoder(32, 32, 32)
-decoder = Decoder()
+encoder = Encoder(env.shift_features, env.count_workers, 32, 32, 32)
+decoder = Decoder(env.count_shifts)
 policy = Policy(encoder, decoder).to(device)
 
 optimizer = optim.Adam(policy.parameters(), lr=1e-3) # 1e-2
@@ -28,13 +37,13 @@ scores = agent.run(env,n_episodes=n_episodes,print_every=1000)
 #scores = agent.run(env, n_episodes)
 
 
-filename = f"Reinforce_SchedulingEnv_{now}_max_t=1000_lr=1e-3_episodes={n_episodes}" #f"Random_SchedulingEnv_{now}"  
+filename = f"Reinforce_SchedEnv_{now}_max_t=1000_lr=1e-3_episodes={n_episodes}_{p}_{s}" #f"Random_SchedulingEnv_{now}"  
 
 x = [i+1 for i in range(n_episodes -1)]
 #plot_scores(scores, filename)
 plot_learning_curve(scores, x, filename)
 
-
+print(f"time to complete: {datetime.now() - start}")
 
 # Tracking
 # mlfow or tensorboard?
@@ -53,4 +62,4 @@ plot_learning_curve(scores, x, filename)
 #   max
 #   count of max?
 #   min
-#   std 
+#   std  """
